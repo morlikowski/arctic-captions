b7a5222737297625a61a030f697281878e35b2fd
Matthias Orlikowski <mazsvr@gmail.com>
2017-07-13 19:54:48 +0200

    Started rewrite of flickr8k preprocessing

--- arcticcaptions/prepare_flickr8k.py
+++ arcticcaptions/prepare_flickr8k.py
@@ -1,39 +1,42 @@
-import sys
+gimport sys
+import os
 
-
-codegit_root = '/home/intuinno/codegit'
-
-sys.path.insert(0, codegit_root)
-
-from anandlib.dl.caffe_cnn import *
 import pandas as pd
 import numpy as np
-import os
+
 import nltk
+from nltk.tokenize import TreebankWordTokenizer
+
 import scipy
 import json
 import cPickle
 from sklearn.feature_extraction.text import CountVectorizer
-from nltk.tokenize import TreebankWordTokenizer
 import pdb
+
+from keras.applications.vgg19 import VGG19
+from keras.preprocessing import image
+from keras.applications.vgg19 import preprocess_input
+from keras.models import Model
+
+from skimage.io import imread
+
+from images import crop_image
 
 TRAIN_SIZE = 6000
 TEST_SIZE = 1000
 
-annotation_path = '/home/intuinno/project/pointTeach/data/Flicker8k/Flickr8k.token.txt'
-vgg_deploy_path = '/home/intuinno/codegit/caffe/models/vgg_ilsvrc_19/VGG_ILSVRC_19_layers_deploy.prototxt'
-vgg_model_path  = '/home/intuinno/codegit/caffe/models/vgg_ilsvrc_19/VGG_ILSVRC_19_layers.caffemodel'
-flickr_image_path = '/home/intuinno/project/pointTeach/data/Flicker8k/preprocessedImages'
-feat_path='feat/flickr8k'
+annotation_path = '../data/flicker8k/text/Flickr8K.token.txt'
+flickr_image_path = '../data/Flicker8k/preprocessedImages/'
+feat_path='../feat/flickr8k/'
 
 def my_tokenizer(s):
     return s.split()
 
-cnn = CNN(deploy=vgg_deploy_path,
-          model=vgg_model_path,
-          batch_size=20,
-          width=224,
-          height=224)
+#cnn = CNN(deploy=vgg_deploy_path,
+#          model=vgg_model_path,
+#          batch_size=20,
+#          width=224,
+#          height=224)
 
 annotations = pd.read_table(annotation_path, sep='\t', header=None, names=['image', 'caption'])
 annotations['image_num'] = annotations['image'].map(lambda x: x.split('#')[1])
@@ -90,9 +93,17 @@ caption_image_id_train = [image_id_dict_train[img] for img in images_train for i
 # Create tuples of caption and image id
 cap_train = zip(captions_train, caption_image_id_train)
 
+# Iterate over all image paths, 100 image paths per iteration
+# TODO: Evaluate if iterating over ranges makes sense - perhaps use to load multiple images at once? Is that faster?
 for start, end in zip(range(0, len(images_train)+100, 100), range(100, len(images_train)+100, 100)):
     image_files = images_train[start:end]
-    feat = cnn.get_features(image_list=image_files, layers='conv5_3', layer_sizes=[512,14,14])
+    # TODO load and crop image, then extract features
+    for image_file in image_files:
+        image = imread(image_file)
+        x = crop_image(image, 244, 244)
+        #feat = model.predict(x)
+        #feat = cnn.get_features(image_list=image_files, layers='conv5_3', layer_sizes=[512,14,14])
+    
     if start == 0:
         feat_flatten_list_train = scipy.sparse.csr_matrix(np.array(map(lambda x: x.flatten(), feat)))
     else:
