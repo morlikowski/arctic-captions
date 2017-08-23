afc47d1f2408074049c3cb73066e3bb1534da8be
Matthias Orlikowski <mazsvr@gmail.com>
2017-08-23 16:48:58 +0200

    Removed errornous image paths completely in download script

--- download_flickr8k.sh
+++ download_flickr8k.sh
@@ -10,8 +10,7 @@ cd text/
 wget http://nlp.cs.illinois.edu/HockenmaierGroup/Framing_Image_Description/Flickr8k_text.zip
 unzip Flickr8k_text.zip
 rm Flickr8k_text.zip
-# There are paths (all for the same file) that end with ".jpg.1". 
-# However, there is no corresponding actual image file, leading to an exception.
-# Therefore, delete the ".1" part of the paths.
+# There are paths that have no corresponding actual image file, leading to an exception.
 # Is this a known issue with Flickr8k?
-sed -i s/\\.1//g Flickr8k.token.txt
+grep -v "2258277193_586949ec62.jpg" Flickr8k.token.txt > Flickr8k.token.txt.new
+mv Flickr8k.token.txt.new Flickr8k.token.txt
